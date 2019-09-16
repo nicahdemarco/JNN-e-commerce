67e8ce412903fcb8dab3c51377be798d460b1c4b
Nicolas <nicahdemarco@gmail.com>
2019-09-16 20:19:02 -0300

    sdfsdf

--- index.php
+++ index.php
@@ -9,13 +9,12 @@
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
     
-	<link rel="stylesheet" href="./CSS/master.css">
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
 
     
-
+	<link rel="stylesheet" href="CSS/master.css">
     <title>JNN-e-commerce</title>
 </head>
 
@@ -126,7 +125,7 @@
 
 		<!-- footer -  Aca ponemos las secciones raras como las preguntas y contactos, etc.-->		
 		
-	</div>
+		</div>
 
 	</body>
 </html>
