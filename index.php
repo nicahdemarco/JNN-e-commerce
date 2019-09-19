577dddb1b072534230d6301b7fa7020695976ebc
JenMartinez98 <jenifermartinez56@gmail.com>
2019-09-18 21:25:30 -0300

    Cualquier cosa

--- index.php
+++ index.php
@@ -1,4 +1,7 @@
-<?php include_once('baseDatos.php');
+<?php 
+	include_once('baseDatos.php');
+
+	
 ?>
 <!-- esto esta linkeado para mas adelante -->
 <!doctype html>
@@ -12,38 +15,54 @@
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
-    <link rel="stylesheet" href="./CSS/master.css">
+    <link rel="stylesheet" href="CSS/master.css">
     
 
     <title>JNN-e-commerce</title>
 </head>
 
-<div class="container">
+<div class="container-fluid">
 
         <!-- En este archivo esta toda la estructura html, a medida que cada seccion se termine,
         hay que reemplazarla con la seccion php insertada -->
 
-
-
-        <!-- cabecera - Aca vamos a tener una navbar que tiene un logo, una barra de busqueda, 
-        un boton de crear cuenta y un boton de ingresar, un icono de carrito -->
+	  <!-- cabecera - Aca vamos a tener una navbar que tiene un logo, una barra de busqueda, 
+		un boton de crear cuenta y un boton de ingresar, un icono de carrito -->
+		
 		<header class="main-header">
-			<img src="images/logo.jpg" alt="logotipo" class="logo">
 
-			<a href="#" class="toggle-nav">
-				<span class="fa fa-bars"></span>
-			</a>
+		<nav class="navbar navbar-expand-lg w-100 ">
+				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+					<span class="navbar-toggler-icon"></span>
+				</button>
+				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+					<a class="navbar-brand" href="#">
+						<img src="img-proyecto/tshirt.png" wihgt="40" height="40" alt="">
+						Brand tshirt
+					</a>
 
-			<nav class="main-nav">
-				<ul>
-					<li><a href="#"> crear cuenta</a></li>
-					<li><a href="#">ingresar</a></li>
-					<li><a href="#">carrito</a></li>
-					
+					<form class="form-inline my-2 my-lg-0 ">
+					<input class="form-control mr-sm-2" type="search" placeholder="buscar" aria-label="Search">
+					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+					</form>
+
+				<ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
+					<li class="nav-item ">
+						<a class="nav-link" href="#">Crear cuenta<span class="sr-only">(current)</span></a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#">Ingresar</a>
+					</li>
+
 				</ul>
-			</nav>
+					
+					<a class="Carrito de Compras" href="#">
+					<img src="img-proyecto/añadir-al-carrito.png" wight="40" height="40" alt="">
+					</a>
+				</div> 
+		</nav>
 		</header>
-
+	
 		<!-- carrousel de productos de tres slides, Bootstrap tiene bastante facil-->
 		<section class="carousel">
 			<img src="images/img-banner.jpg" alt="banner">
