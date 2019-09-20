0216eecdff0d2e24b4fb3eeaee5926471b37336d
nicochapur <54456630+nicochapur@users.noreply.github.com>
2019-09-20 02:06:41 -0300

    footer

--- index.php
+++ index.php
@@ -8,7 +8,7 @@
     <!-- Required meta tags -->
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
-    
+    <script src="https://kit.fontawesome.com/a27597434b.js" crossorigin="anonymous"></script>
     <!-- Bootstrap CSS -->
     <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css"
         integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
@@ -144,6 +144,78 @@
         
 
 		<!-- footer -  Aca ponemos las secciones raras como las preguntas y contactos, etc.-->		
+		
+		<footer class="main-footer row">
+		<hr class="mt-0 d-inline-block mx-auto " style="width:95%;">
+			<div>
+				<ul class="redeslist">
+					<li><H5>REDES SOCIALES</H5></li>
+
+					<li><hr class="accent-2 mt-0 d-inline-block mx-auto" style="width: 60px;"></li>
+
+					<span><a href=""><i class="fab fa-facebook-f white-text mr-1 fa-2x"> </i></a></span>
+					<!-- Twitter -->
+					<span><a href=""><i class="fab fa-twitter white-text mr-1 fa-2x"> </i></a></span>
+					<!-- Google +-->
+					<span><a href=""><i class="fab fa-google-plus-g white-text mr-1 fa-2x"> </i></a></span>
+					<!--Linkedin -->
+					<span><a href=""><i class="fab fa-linkedin-in white-text mr-1 fa-2x"> </i></a></span>
+					<!--Instagram-->
+					<span><a href=""><i class="fab fa-instagram white-text mr-1 fa-2x"> </i></a></span>
+					<!--Pinterest-->
+					<span><a href=""><i class="fab fa-pinterest white-text mr-1 fa-2x"> </i></a></span>
+				</ul>
+			</div>
+
+			<div>
+				<ul class="metodlist">
+					<li><h5>METODOS DE PAGO</h5></li>
+
+					<li><hr class="accent-2 mt-0 d-inline-block mx-auto" style="width: 60px;"></li>
+
+					<span><a href="#" target="_blank"><img src="img-proyecto/Visa_Logo.png" class="mr-1" width="40px"></i></a></span>
+					<span><a href="#" target="_blank"><img src="img-proyecto/mastercard.png" class="mr-1" width="40px"></a></span>
+					<span><a href="#" target="_blank"><img src="img-proyecto/pagofacil.png" class="mr-1" width="40px"></a></span>
+					<span><a href="#" target="_blank"><img src="img-proyecto/paypal.png" class="mr-1" width="60px"></a></span>
+				</ul>
+			</div>
+
+			<div>
+				<ul class="envioslist">
+					<li><h5>ENVIOS</h5></li>
+
+					<li><hr class="accent-2 mt-0 d-inline-block mx-auto" style="width: 60px;"></li>
+
+					<span><a href="#" target="_blank"><img src="img-proyecto/oca.png" class="mr-1" width="35px"></a></span>
+					<span><a href="#" target="_blank"><img src="img-proyecto/logo-mercadoenvios.png" class="mr-1" width="50px"></a></span>
+					<span><a href="#" target="_blank"><img src="img-proyecto/correoarg.png" class="mr-1" width="65px"></a></span>
+				</ul>
+			</div>
+
+			<div>
+				<ul class="contactlist">
+					<li><h5>CONTACTO</h5></li>
+
+					<hr class="accent-2 mt-0 d-inline-block mx-auto" style="width: 60px;">
+
+					<li><a href="mailto:JNN-ecommerce@gmail.com" target="_blank"><i class="fas fa-envelope">  </i>  JNN-ecommerce@gmail.com</a></li>
+					<li><a href="https://www.google.com.ar/maps/place/Av.+Monroe+860,+Buenos+Aires/@-34.5488518,-58.4458325,17z/data=!3m1!4b1!4m5!3m4!1s0x95bcb436f18dc22f:0x36090a5d367889c6!8m2!3d-34.5488518!4d-58.4436438" target="_blank"><i class="fas fa-map-marker-alt">  </i>  Retira tu pedido por Belgrano (Capital Federal)</a></li>
+					<li><a href="tel:+54666666"><i class="fas fa-phone-square-alt">  +54 666 666</i></a></li>
+				</ul>
+			</div>
+			
+			<hr class="mt-0 d-inline-block mx-auto " style="width:95%;">
+
+		
+			<div class="pie">
+				<ul>
+					<span class="mr-3">© 2018 Copyright</span>
+					<span class="mr-1"><a href="#">   Términos y Condiciones</a> |</span>
+					<span class="mr-1"><a href="#">Politicas de Privacidad</a> |</span>
+					<span class="mr-1"><a href="#">Ayuda</a></span>
+				</ul>
+			</div>
+		</footer>	
 		
 		</div>
 
