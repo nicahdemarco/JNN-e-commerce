231ace29fec7a70e21777bf545e3a96f195ba40c
JenMartinez98 <jenifermartinez56@gmail.com>
2019-09-20 07:04:02 -0300

    Modificacion en navbar.php y estilo en navegador.css

--- navbar.php
+++ navbar.php
@@ -1,30 +1,29 @@
 <nav class="navbar navbar-expand-lg w-100 ">
+
 				<button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
 					<span class="navbar-toggler-icon"></span>
-				</button>
+                </button>
+                
 				<div class="collapse navbar-collapse" id="navbarTogglerDemo01">
 					<a class="navbar-brand" href="#">
-						<img src="img-proyecto/tshirt.png" wihgt="40" height="40" alt="">
+						<img src="img-proyecto/tshirt.png" wihgt="50" height="50" alt="">
 						Brand
 					</a>
-
-					<form class="form-inline my-2 my-lg-0 ">
-					<input class="form-control mr-sm-2" type="search" placeholder="buscar" aria-label="Search">
-					<button class="btn btn-outline-success my-2 my-sm-0" type="submit">Buscar</button>
+                <div class="buscador">
+					<form class="form-inline mb-3 my-2 my-lg-0 " widht="60">
+                    <input type="text" class="form-control" aria-label="Sizing example input" aria-describedby="inputGroup-sizing-lg">
 					</form>
+                </div>
 
-				<ul class="navbar-nav mr-auto mt-2 mt-lg-0 ">
-					<li class="nav-item ">
-						<a class="nav-link" href="#">Crear cuenta<span class="sr-only">(current)</span></a>
-					</li>
-					<li class="nav-item">
-						<a class="nav-link" href="#">Ingresar</a>
-					</li>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="cuenta">Crear Cuenta</button>
+                    <button class="btn btn-outline-success my-2 my-sm-0" type="submit" id="ingresar">Ingresar</button>
 
-				</ul>
+                    <a class="usuario" href="#" >
+					<img src="img-proyecto/usuario.png" wight="40" height="40" alt="" >
+					</a>
 					
-					<a class="Carrito de Compras" href="#">
-					<img src="img-proyecto/añadir-al-carrito.png" wight="40" height="40" alt="">
+					<a class="Carrito" href="#" >
+					<img src="img-proyecto/carrito-de-compras.png" wight="40" height="40" alt="" >
 					</a>
 				</div> 
 		</nav>
