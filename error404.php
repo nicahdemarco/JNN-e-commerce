09b544a139666778a3247c03cd0e3fd2acfe38e4
nicochapur <54456630+nicochapur@users.noreply.github.com>
2019-09-26 14:33:47 -0300

    Agrego pag 404 con css

---
+++ error404.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <link rel="stylesheet" href="CSS/404.css">
+    <title>Document</title>
+</head>
+<body>
+    <ul>
+        <li>
+            <p>Está página no existe o no funciona correctamente</p>
+        </li>
+        <br>
+        <br>
+        <li>
+            <p>
+                Inténtelo más tarde o vuelva al
+                <a href="index.php">Inicio</a>
+            </p> 
+        </li>
+    </ul>
+    
+</body>
+</html>
